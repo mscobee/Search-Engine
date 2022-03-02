430a371d18d7ec390ff460b72a74713af0248dee
mscobee <michael.c.scobee@gmail.com>
2022-03-01 20:30:57 -0500

    added some docstrings

--- engine/indexerp/indexer.py
+++ engine/indexerp/indexer.py
@@ -1,6 +1,7 @@
 import databasep, os
 from pathlib import Path
 class Indexer:
+    """Indexer is used to index the web pages that the Crawler crawls"""
     def __init__(self) -> None:
         self.new_file = None
         self.dbc = databasep.DatabaseController(
@@ -16,16 +17,21 @@ class Indexer:
 
 
     def set_data(self, title):
+        """Set the title of the crawled web page, the file name that is to be saved and define the path of the file."""
         self.title = title
         self.file_name = title + '.html'
         self.dbc.set_title('test')
         self.abs_path = os.path.join(self.dir_path, self.file_name)
 
+
     def insert(self):
+        """Inserts the web page title and index location into the database."""
         self.dbc.insert_data()
 
 
     def create_file(self, content):
+        """Creates a file in the target directory if it does not exist and writes the html content of the scraped
+        website to the file."""
         self.new_file = Path(self.abs_path)
         self.new_file.touch(exist_ok=True)
         self.new_file = open(self.abs_path, 'wb')
