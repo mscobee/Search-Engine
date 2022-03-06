5d8ad66e3d13659c4d355d084f35d7c91ca3e503
mscobee <michael.c.scobee@gmail.com>
2022-03-05 19:45:30 -0500

    added stop word removal to HtmlCleaner class

--- engine/crawlerp/cleaner.py
+++ engine/crawlerp/cleaner.py
@@ -1,14 +1,34 @@
-import nltk
+from nltk.stem import PorterStemmer
 from urllib.request import urlopen
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
 class HtmlCleaner:
 
 
     def __init__(self) -> None:
-        pass
+        self.ps = PorterStemmer()
+        self.stop_words = set(stopwords.words('english'))
 
-    def remove_html_tags(self, url) -> str:
-        html = urlopen(url).read()
-        return nltk.clean_html(html)
+        
+    def remove_stop_words(self):
+        new_text = ''
+        for w in self.content:
+            if w not in self.stop_words:
+                new_text += w + ' '
+            self.set_data(new_text) # this is shit i think? find more efficient way to do the data transfers
+    def stemming(self):
+        new_text = ''
+        for word in self.content:
+            new_text += (self.ps.stem(word) + ' ')
+        self.content = new_text
+
+    def set_data(self, content):
+        # split the string content into a list on all whitespace and '|'
+        self.content = re.split('\||\s', content)
+    def run_clean(self):
+        self.content = self.__stemming()
+        return self.content
     
 
 
--- engine/main.py
+++ engine/main.py
@@ -3,14 +3,26 @@ import indexerp
 import re
 def main():
     """Main function driver"""
-    initial_url = 'https://tbc.wow-tools.com/tbc_arena_calculator'
+    initial_url = 'http://localhost:8080/'
     webpage = crawlerp.WebPage()
     crawler = crawlerp.Crawler()
     indexer = indexerp.Indexer()
+    cleaner = crawlerp.HtmlCleaner()
 
     webpage.set_page_data(initial_url)
-    crawler.crawl(indexer, webpage)
-    crawler.print_queue()
+    # crawler.crawl(indexer, webpage)
+    cleaner.set_data(webpage.get_content())
+    print('Before')
+    print(cleaner.content)
+
+    print('After stopword removal')
+    cleaner.remove_stop_words()
+    print(cleaner.content)
+
+    print('after stemming')
+    cleaner.stemming()
+    print(cleaner.content)
+    
 
 if __name__ == "__main__":
     main()
